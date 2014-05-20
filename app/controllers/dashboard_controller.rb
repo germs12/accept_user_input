9939a587c3ed537fedd6699e4ecd2b097b64f783
James Thullbery <ifndef@gmail.com>
2014-05-20 14:16:33 -0400

    example for ethan

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,14 @@
 class DashboardController < ApplicationController
 
   def index
-
+    @result = nil
+    if params['user_input']
+      api_key = 'd8372703aeb94b7035ce12d07f61ba84'
+      api_id = 'ceadf6d2'
+      query = params['user_input']
+      api_url = "https://api.nutritionix.com/v1_1/search/#{query}?results=0:01&fields=item_name,brand_name,item_id,nf_calories,nf_carbohydrates,nf_protein,nf_carbs&appId=#{api_id}&appKey=#{api_key}"
+      @result = HTTParty.get(api_url)
+    end
   end
 
   def submit
