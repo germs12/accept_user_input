696d68393c50d9d8ae3306ddfd8a0e3dc494d45c
James Thullbery <ifndef@gmail.com>
2014-05-20 14:48:16 -0400

    partial

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,6 @@ class DashboardController < ApplicationController
   end
 
   def submit
-    binding.pry
     call_api(params['user_input_form']['user_input'])
   end
 
@@ -15,7 +14,7 @@ class DashboardController < ApplicationController
       api_key = 'd8372703aeb94b7035ce12d07f61ba84'
       api_id = 'ceadf6d2'
       query = input
-      api_url = "https://api.nutritionix.com/v1_1/search/#{query}?results=0:01&fields=item_name,brand_name,item_id,nf_calories,nf_carbohydrates,nf_protein,nf_carbs&appId=#{api_id}&appKey=#{api_key}"
+      api_url = "https://api.nutritionix.com/v1_1/search/#{query}?results=0:10&fields=item_name,brand_name,item_id,nf_calories,nf_carbohydrates,nf_protein,nf_carbs&appId=#{api_id}&appKey=#{api_key}"
       @result = HTTParty.get(api_url)
     end
   end
